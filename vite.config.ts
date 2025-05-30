203ee2866ae9880f0af88b1abcc79cc1ff1548a8
Niel Thiart <niel@thiart.com>
2025-05-30 15:16:19 +0200

    Use env vars for fb app id and url

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.BASE_PATH || '/',
-  plugins: [],
   build: {
     rollupOptions: {
       external: ['fs'],
